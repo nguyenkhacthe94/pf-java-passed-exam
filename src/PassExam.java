a357c6c124e4eaac66f9cccc244fe8cd582fbc1f
nguyenkhacthe94 <nguyenkhacthe94@gmail.com>
2018-03-22 16:12:23 +0700

    Passed exam students

---
+++ src/PassExam.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PassExam {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int size;
+        int[] array;
+        do {
+            System.out.print("Enter a size:");
+            size = input.nextInt();
+            if (size > 30 || size < 1)
+                System.out.println("Size should not exceed 30");
+        } while (size > 30 || size < 1);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter a mark for student " + (i + 1) + ": ");
+            array[i] = input.nextInt();
+            i++;
+        }
+        int count = 0;
+        System.out.print("List of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.print("\n The number of students passing the exam is " + count);
+    }
+}
